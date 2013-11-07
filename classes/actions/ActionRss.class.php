107463902f5df8558c6d01002159c01d7236f204
wasja1982 <wasja@yandex.ru>
2013-11-07 20:59:31 +0300

    Исправлена ошибка с генерацией RSS для комментариев.

--- classes/actions/ActionRss.class.php
+++ classes/actions/ActionRss.class.php
@@ -8,29 +8,36 @@
  **/
 
 /**
- * Обрабатывает RSS
+ * Экшен бработки RSS
  * Автор класса vovazol(http://livestreet.ru/profile/vovazol/)
  *
  */
 class PluginOpencomments_ActionRss extends PluginOpencomments_Inherit_ActionRss {
-
-
+	/**
+	 * Вывод RSS последних комментариев
+	 */
 	protected function RssComments() {
 		/**
-		 * Вычисляем топики из закрытых блогов, чтобы исключить их из выдачи
+		 * Получаем закрытые блоги, чтобы исключить их из выдачи
 		 */
-		$aCloseTopics = $this->Topic_GetTopicsCloseByUser();		
-		
-		$aResult=$this->Comment_GetCommentsAll('topic',1,Config::Get('module.comment.per_page')*2,$aCloseTopics);
+		$aCloseBlogs = $this->Blog_GetInaccessibleBlogsByUser();
+		/**
+		 * Получаем комментарии
+		 */
+		$aResult=$this->Comment_GetCommentsAll('topic',1,Config::Get('module.comment.per_page')*2,array(),$aCloseBlogs);
 		$aComments=$aResult['collection'];
-		
+		/**
+		 * Формируем данные канала RSS
+		 */
 		$aChannel['title']=Config::Get('view.name');
 		$aChannel['link']=Config::Get('path.root.web');
 		$aChannel['description']=Config::Get('path.root.web').' / RSS channel';
 		$aChannel['language']='ru';
 		$aChannel['managingEditor']=Config::Get('general.rss_editor_mail');
 		$aChannel['generator']=Config::Get('path.root.web');
-		
+		/**
+		 * Формируем записи RSS
+		 */
 		$comments=array();
 		foreach ($aComments as $oComment){
 			$item['title']='Comments: '.$oComment->getTarget()->getTitle();
@@ -46,30 +53,42 @@ class PluginOpencomments_ActionRss extends PluginOpencomments_Inherit_ActionRss 
 			$item['category']='comments';
 			$comments[]=$item;
 		}
-		
+		/**
+		 * Формируем ответ
+		 */
 		$this->InitRss();
 		$this->Viewer_Assign('aChannel',$aChannel);
 		$this->Viewer_Assign('aItems',$comments);
 		$this->SetTemplateAction('index');
 	}
-
+	/**
+	 * Вывод RSS комментариев конкретного топика
+	 */
 	protected function RssTopicComments() {
 		$sTopicId=$this->GetParam(0);
-		
+		/**
+		 * Топик существует?
+		 */
 		if (!($oTopic=$this->Topic_GetTopicById($sTopicId)) or !$oTopic->getPublish() or $oTopic->getBlog()->getType()=='close') {
 			return parent::EventNotFound();
 		}
-		
-		$aComments=$this->Comment_GetCommentsByTargetId($oTopic->getId(),'topic');
-		$aComments=$aComments['comments'];
-		
+		/**
+		 * Получаем комментарии
+		 */
+		$aResult=$this->Comment_GetCommentsByFilter(array('target_id'=>$oTopic->getId(),'target_type'=>'topic','delete'=>0),array('comment_id'=>'desc'),1,100);
+		$aComments=$aResult['collection'];
+		/**
+		 * Формируем данные канала RSS
+		 */
 		$aChannel['title']=Config::Get('view.name');
 		$aChannel['link']=Config::Get('path.root.web');
 		$aChannel['description']=Config::Get('path.root.web').' / RSS channel';
 		$aChannel['language']='ru';
 		$aChannel['managingEditor']=Config::Get('general.rss_editor_mail');
 		$aChannel['generator']=Config::Get('path.root.web');
-		
+		/**
+		 * Формируем записи RSS
+		 */
 		$comments=array();
 		foreach ($aComments as $oComment){
 			$item['title']='Comments: '.$oTopic->getTitle();
@@ -85,12 +104,13 @@ class PluginOpencomments_ActionRss extends PluginOpencomments_Inherit_ActionRss 
 			$item['category']='comments';
 			$comments[]=$item;
 		}
-		
+		/**
+		 * Формируем ответ
+		 */
 		$this->InitRss();
 		$this->Viewer_Assign('aChannel',$aChannel);
 		$this->Viewer_Assign('aItems',$comments);
-		$this->SetTemplateAction('index');	
+		$this->SetTemplateAction('index');
 	}
-	
 }
 ?>
